8a5c74072ff30ca8d7bfa1e5d5106b9b8a7b332b
gokulg3107-sudo <gokulg3107@gmail.com>
2026-05-15 11:10:20 +0530

    Remove all files

--- inc.h
+++
@@ -1,4 +0,0 @@
-`define baud_rate 2400
-`define clk 50000000
-`define width 8
-`define cw ($clog2(`clk / (`baud_rate * 2 * 16)))
--- tb.v
+++
@@ -1,4 +0,0 @@
-module tb;
-reg uart_clk, sys_rst, xmitH;
-reg [`width - 1: 0] xmit_dataH;
-wire xmit_doneH, uart_XMIT_dataH;
